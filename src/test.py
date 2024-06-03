f7499bdb8cc614b67c971598681dc53241289f49
Steven Pestana <650301+spestana@users.noreply.github.com>
2024-06-03 08:26:17 -0700

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,10 @@
+import xarray as xr
+import goes_ortho as go
+import geogif
+
+print("open text file")
+
+with open('test.txt', 'w') as f:
+    f.write('hello world')
+
+print("wrote to text file")
